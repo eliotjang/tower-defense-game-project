f0e1c3072cdbab60e14e046b849f9d96fe17324a
Dongkyun Kim <donkim0122@gmail.com>
2024-06-17 22:51:08 +0900

    Update: import tower.json, use lvl 1 tower's data on create Update: 'setStats' to set tower stats based on given level (param) Update: tower cooldown resets to the cooldown set in the asset 'tower.json'

--- public/src/tower.js
+++ public/src/tower.js
@@ -1,16 +1,37 @@
+import towerData from '../assets/tower.json' with {type:'json'};;
+
 export class Tower {
-  constructor(x, y, cost) {
+  constructor(x, y) {
     // 생성자 안에서 타워들의 속성을 정의한다고 생각하시면 됩니다!
     this.x = x; // 타워 이미지 x 좌표
     this.y = y; // 타워 이미지 y 좌표
-    this.width = 78; // 타워 이미지 가로 길이 (이미지 파일 길이에 따라 변경 필요하며 세로 길이와 비율을 맞춰주셔야 합니다!)
-    this.height = 150; // 타워 이미지 세로 길이
-    this.attackPower = 40; // 타워 공격력
-    this.range = 300; // 타워 사거리
-    this.cost = cost; // 타워 구입 비용
-    this.cooldown = 0; // 타워 공격 쿨타임
-    this.beamDuration = 0; // 타워 광선 지속 시간
+    this.setStats(1);
     this.target = null; // 타워 광선의 목표
+  }
+
+  getLevel() {
+    return this.level;
+  }
+
+  setStats (level) {
+    this.level = level; // 타워 레벨
+    const index = this.level - 1;
+    this.width = towerData.data[index].width; // 타워 이미지 가로 길이 (이미지 파일 길이에 따라 변경 필요하며 세로 길이와 비율을 맞춰주셔야 합니다!)
+    this.height = towerData.data[index].height; // 타워 이미지 세로 길이
+    this.attackPower = towerData.data[index].attack_power; // 타워 공격력
+    this.range = towerData.data[index].range; // 타워 사거리
+    this.cost = towerData.data[index].cost; // 타워 구입 비용
+    this.cooldown = towerData.data[index].cooldown; // 타워 공격 쿨타임
+    this.beamDuration = towerData.data[index].beamDuration; // 타워 광선 지속 시간
+  }
+
+  /**
+   * TODO: 업그레이드 기능, 서버로 이벤트 전송
+   */
+  upgrade() {
+    // 바로 하지말고 서버 갔다와서 성공 시에만 핸들링 하기
+    // this.setStats(this.level + 1);
+    // sendEvent()
   }
 
   draw(ctx, towerImage) {
@@ -34,8 +55,8 @@ export class Tower {
     // 타워가 타워 사정거리 내에 있는 몬스터를 공격하는 메소드이며 사정거리에 닿는지 여부는 game.js에서 확인합니다.
     if (this.cooldown <= 0) {
       monster.hp -= this.attackPower;
-      this.cooldown = 180; // 3초 쿨타임 (초당 60프레임)
-      this.beamDuration = 30; // 광선 지속 시간 (0.5초)
+      this.cooldown = towerData.data[this.level - 1].cooldown; // 3초 쿨타임 (초당 60프레임)
+      this.beamDuration = towerData.data[this.level - 1].beamDuration; // 광선 지속 시간 (0.5초)
       this.target = monster; // 광선의 목표 설정
     }
   }
