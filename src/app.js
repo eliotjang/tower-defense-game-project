bdea28294d158fecf60f82bd03b1375f89443730
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-06-18 12:12:30 +0900

    Fix 소켓 요청 및 응답 방식

--- src/app.js
+++ src/app.js
@@ -2,18 +2,11 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import config from './utils/configs.js';
-import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
 const PORT = config.serverPort;
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 // static file(html, css, js) serve middleware
 app.use(express.static('public'));
 // body parser middleware
