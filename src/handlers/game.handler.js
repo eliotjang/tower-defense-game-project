24b3cede508ee47f296039fab71e3de553bc9f7a
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-06-18 09:59:17 +0900

    Update 게임 시작 이벤트

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,9 +1,14 @@
-export const gameStart = (uuid, payload) => {
+export const gameStart = (uuid, payload, socket) => {
+  // socket emit test
   const { timeStamp } = payload;
 
   if (false) {
     return { status: 'fail', message: '게임 시작 실패' };
   }
+
+  console.log(timeStamp);
+
+  socket.emit('gameStart', { message: '게임 시작 시간 정보입니다.', timeStamp });
 
   return { status: 'success', message: '게임 시작!' };
 };
