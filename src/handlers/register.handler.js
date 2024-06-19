7d5c8ac5915461b3889bf5d2c40814f296188304
Dongkyun Kim <donkim0122@gmail.com>
2024-06-19 21:09:47 +0900

    Update: io connection is now wrapped in try/catch for error handling Update: jwt verification throws CustomError on error

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -4,41 +4,42 @@ import jwt from 'jsonwebtoken';
 import { userRedis } from '../utils/redis.utils.js';
 import redisClient from '../init/redis.js';
 import { RedisFlushModes } from 'redis';
+import { handleError } from './error.handler.js';
+import CustomError from '../utils/errors/classes/custom.error.js';
 
 const registerHandler = (io) => {
   io.on('connection', async (socket) => {
     // 최초 커넥션을 맺은 이후 발생하는 각종 이벤트를 처리하는 곳
-    const token = socket.handshake.auth.token;
-    const jwtSecret = configs.jwtSecret;
-    if (!token) {
-      //토큰이 없으면 연결 종료
-      socket.emit('unauthorized', '토큰이 존재하지 않습니다 로그인을 해 주세요');
-      socket.disconnect(true);
-      return;
-    }
     try {
-      const decoded = jwt.verify(token, jwtSecret);
-      console.log('JWT 검증 성공');
-      socket.data = {
-        user_id: decoded.user_id,
-        isAuthenticated: true,
-      }; //인증된 사용자의 상태를 저장
-    } catch (error) {
-      console.error('JWT 검증중 오류 발생', error);
-      socket.disconnect(true);
+      const token = socket.handshake.auth.token;
+      const jwtSecret = configs.jwtSecret;
+      if (!token) {
+        //토큰이 없으면 연결 종료
+        throw new CustomError('토큰이 존재하지 않습니다 로그인을 해 주세요', 'unauthorized');
+      }
+      jwt.verify(token, jwtSecret, (err, tokenPayload) => {
+        if (err) throw new CustomError('JWT 검증중 오류 발생', 'unauthorized');
+        console.log('JWT 검증 성공');
+        socket.data = {
+          user_id: tokenPayload.user_id,
+          isAuthenticated: true,
+        }; //인증된 사용자의 상태를 저장
+      });
+      const USER_KEY_PREFIX = 'user:';
+      const key = USER_KEY_PREFIX + socket.data.user_id;
+      const UUID = await redisClient.hGet(key, 'uuid');
+      const sanitizedUUID = UUID.replace(/"/g, '');
+
+      handleConnection(socket, sanitizedUUID);
+
+      // 모든 서비스 이벤트 처리
+      socket.on('event', (data) => handleEvent(io, socket, data));
+
+      // 접속 해제시 이벤트 처리
+      socket.on('disconnect', () => handleDisconnect(socket, sanitizedUUID));
+    } catch (err) {
+      await handleError(err, socket);
     }
-    const USER_KEY_PREFIX = 'user:';
-    const key = USER_KEY_PREFIX + socket.data.user_id;
-    const UUID = await redisClient.hGet(key, 'uuid');
-    const sanitizedUUID = UUID.replace(/"/g, '');
-
-    handleConnection(socket, sanitizedUUID);
-
-    // 모든 서비스 이벤트 처리
-    socket.on('event', (data) => handleEvent(io, socket, data));
-
-    // 접속 해제시 이벤트 처리
-    socket.on('disconnect', () => handleDisconnect(socket, sanitizedUUID));
   });
 };
 
