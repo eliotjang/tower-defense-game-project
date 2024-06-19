47054cb87126d0ffbdab6500b1fa6b4f639de336
4cozm <17328rm@gmail.com>
2024-06-19 19:30:10 +0900

    update redis에 killcount 증감 추가

--- src/handlers/monster.handler.js
+++ src/handlers/monster.handler.js
@@ -1,20 +1,7 @@
 import { getGameAssets } from '../init/assets.js';
 import { gameRedis } from '../utils/redis.utils.js';
 import { constants } from '../constants.js';
-
-export const monsterKillHandler = (uuid, payload, socket) => {
-  const { score: currentScore } = payload;
-
-  if (false) {
-    socket.emit('monsterKill', { status: 'fail', message: '몬스터 처치 검증 실패' });
-    return;
-  }
-
-  socket.emit('monsterKill', { status: 'success', message: '몬스터 처치 성공', score });
-import { getGameAssets } from '../init/assets.js';
-import { gameRedis } from '../utils/redis.utils.js';
-const userData = []; //유저의 정보를 소켓id 기반 인메모리 방식으로 임시 저장
-
+const userData = [];
 const getUser = (socketId) => {
   return userData.find((user) => user.socketId === socketId);
 };
@@ -24,13 +11,12 @@ const initializeUser = (socketId) => {
   let user = getUser(socketId);
   if (!user) {
     user = {
-      count: 0,
       socketId: socketId,
       killLog: [],
     };
     userData.push(user);
     console.log(`유저 정보가 생성되었습니다 socketId: ${socketId}`);
-  } 
+  }
   return user;
 };
 
@@ -44,32 +30,29 @@ export const monsterKillHandler = async (uuid, payload, socket) => {
     let currentStageId; //redis 데이터 기준 유저의 현재 스테이지
     let monsterList; //현재 있는 스테이지에 출현 가능한 몬스터 목록
     let stageField; //현재 스테이지의 정보
-    await gameRedis.getGameData(uuid).then((user) => {
-      if (!user) {
-        throw new Error('정보를 찾을 수 없습니다');
-      }
-      currentStageId = user.stage_id;
-      monsterList = monster_unlock.data.find((item) => item.stage_id == currentStageId).monster;
-      stageField = stage.data.find((item) => item.id == currentStageId).target_score;
-      console.log('현재 점수', user.score);
-      console.log('현재 스테이지', currentStageId);
-      if (monsterList.includes(monsterId)) {
-        
-        const addScore = user.score + score;
-        console.log("추가되는 점수",addScore);
-        gameRedis.patchGameData(uuid, score, addScore); // 몬스터 존재시 점수 증감
-        inMemoryUserData.killLog.push({ monsterId, timeStamp }); //킬로그에 저장
-        inMemoryUserData.count++;
-      } else {
-        console.log('처치 검증 실패', monsterId, '가 처치됨');
-        socket.emit('monsterKill', { status: 'fail', message: '몬스터 처치 검증 실패' });
-        return;
-      }
-      if (stageField < user.score) {
-        gameRedis.patchGameData(uuid, stage_id, user.stage_id + 1);
-      }
-      socket.emit('monsterKill', { status: 'success', message: '몬스터 처치 성공', timeStamp });
-    });
+    const user = await gameRedis.getGameData(uuid);
+    if (!user) {
+      throw new Error('정보를 찾을 수 없습니다');
+    }
+    currentStageId = user.stage_id;
+    monsterList = monster_unlock.data.find((item) => item.stage_id == currentStageId).monster;
+    stageField = stage.data.find((item) => item.id == currentStageId).target_score;
+    console.log('현재 점수', user.score);
+    if (monsterList.includes(monsterId)) {
+      const addScore = user.score + score;
+      await gameRedis.patchGameDataEx(uuid, { score: addScore }); // 몬스터 존재시 점수 증감
+      inMemoryUserData.killLog.push({ monsterId, timeStamp }); //킬로그에 저장
+      await gameRedis.patchGameDataEx(uuid,{kill_count:user.kill_count+1})
+    } else {
+      console.log('처치 검증 실패', monsterId, '가 처치됨');
+      socket.emit('monsterKill', { status: 'fail', message: '몬스터 처치 검증 실패' });
+      return;
+    }
+    if (stageField < user.score) {
+      await gameRedis.patchGameDataEx(uuid, { stage_id: user.stage_id + 1 });
+      console.log('스테이지 이동! 현재 스테이지', currentStageId - 98);
+    }
+    socket.emit('monsterKill', { status: 'success', message: '몬스터 처치 성공', timeStamp });
   } catch (error) {
     console.log({ errorMessage: error.message });
   }
